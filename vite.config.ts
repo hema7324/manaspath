e1eaa266308a4bde025d5311220680c151c27c8f
hema7324 <hema.24bca7324@vitapstudent.ac.in>
2025-08-04 17:01:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
-    base:"manaspath"
+    base: '/'
   ].filter(Boolean),
   resolve: {
     alias: {
